c9823dba55cd5fc38f9dfd9fb81eaa8f8740ba7b
Steven Cherian <stevencherian.va@gmail.com>
2022-09-21 16:16:40 -0400

    Adding demo video for PickMeUp

--- scripts.js
+++ scripts.js
@@ -1,3 +1,17 @@
 function openEmail() {
     window.open('mailto:stevencherian.va@gmail.com')
 }
+
+const videoButton = document.querySelector('.video-button');
+
+const videoContainer = document.querySelector('.video-container');
+
+const close = document.querySelector('.close');
+
+videoButton.addEventListener('click', () => {
+    videoContainer.classList.add('show');
+})
+
+close.addEventListener('click', () => {
+    videoContainer.classList.remove('show');
+})
